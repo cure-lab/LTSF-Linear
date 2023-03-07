59f4bb5bf29f51294e9a21e84ef6e8b87a6ffb39
mixiancmx <83197963+mixiancmx@users.noreply.github.com>
2023-03-07 11:17:20 +0800

    Update run_stat.py

--- run_stat.py
+++ run_stat.py
@@ -1,7 +1,7 @@
 import argparse
 import os
 import torch
-from exp.exp_naive import Exp_Main
+from exp.exp_stat import Exp_Main
 import random
 import numpy as np
 
@@ -15,6 +15,9 @@ parser = argparse.ArgumentParser(description='Autoformer & Transformer family fo
 
 # basic config
 parser.add_argument('--is_training', type=int, required=True, default=1, help='status')
+parser.add_argument('--train_only', type=bool, required=False, default=False, help='Not implemented')
+parser.add_argument('--embed', type=str, default='timeF',
+                    help='Not implemented')
 parser.add_argument('--model_id', type=str, required=True, default='test', help='model id')
 parser.add_argument('--model', type=str, required=True, default='Autoformer',
                     help='model name, options: [Autoformer, Informer, Transformer]')
@@ -28,28 +31,22 @@ parser.add_argument('--features', type=str, default='M',
 parser.add_argument('--target', type=str, default='OT', help='target feature in S or MS task')
 parser.add_argument('--checkpoints', type=str, default='./checkpoints/', help='location of model checkpoints')
 parser.add_argument('--sample', type=float, default=1, help='Sampling percentage, the inference time of ARIMA and SARIMA is too long, you might sample 0.01')
-
+parser.add_argument('--freq', type=str, default='h',
+                    help='freq for time features encoding, options:[s:secondly, t:minutely, h:hourly, d:daily, b:business days, w:weekly, m:monthly], you can also use more detailed freq like 15min or 3h')
+                    
 # forecasting task
 parser.add_argument('--seq_len', type=int, default=96, help='input sequence length')
 parser.add_argument('--label_len', type=int, default=48, help='start token length') # Just for reusing data loader
 parser.add_argument('--pred_len', type=int, default=96, help='prediction sequence length')
 
 # optimization
-parser.add_argument('--num_workers', type=int, default=10, help='data loader num workers')
+parser.add_argument('--num_workers', type=int, default=10, help='Not implemented')
 parser.add_argument('--itr', type=int, default=2, help='experiments times')
 parser.add_argument('--batch_size', type=int, default=100, help='batch size of train input data')
 parser.add_argument('--des', type=str, default='test', help='exp description')
 
 args = parser.parse_args()
-
-args.use_gpu = True if torch.cuda.is_available() and args.use_gpu else False
-args.output_attention = False
-if args.use_gpu and args.use_multi_gpu:
-    args.dvices = args.devices.replace(' ', '')
-    device_ids = args.devices.split(',')
-    args.device_ids = [int(id_) for id_ in device_ids]
-    args.gpu = args.device_ids[0]
-
+args.use_gpu = False
 print('Args in experiment:')
 print(args)
 Exp = Exp_Main
