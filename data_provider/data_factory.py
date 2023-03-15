6fe4c28ff36b4228792f2bbe513e807577e4a57e
mixiancmx <83197963+mixiancmx@users.noreply.github.com>
2023-03-15 23:51:48 +0800

    Update data_factory.py

--- data_provider/data_factory.py
+++ data_provider/data_factory.py
@@ -17,7 +17,7 @@ def data_provider(args, flag):
 
     if flag == 'test':
         shuffle_flag = False
-        drop_last = True
+        drop_last = False
         batch_size = args.batch_size
         freq = args.freq
     elif flag == 'pred':
