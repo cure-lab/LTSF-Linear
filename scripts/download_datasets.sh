8378451163b70273c230d19d31befc72ebb6499c
Mahdi Lamb <mahdilamb@gmail.com>
2023-09-10 16:53:21 +0100

    Fix download path

--- scripts/download_datasets.sh
+++ scripts/download_datasets.sh
@@ -3,7 +3,7 @@
 if [ ! -d ./dataset ]; then
     pip freeze | grep "^gdown" || pip install gdown
     gdown "1alE33S1GmP5wACMXaLu50rDIoVzBM4ik"
-    unzip -n -j all_six_datasets.zip -d datasets
+    unzip -n -j all_six_datasets.zip -d dataset
     rm dataset/.*
     rm all_six_datasets.zip
 fi
