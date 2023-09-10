cc02226b59ddd909f77ffb5e9016a82b1d4cda4f
Mahdi Lamb <mahdilamb@gmail.com>
2023-09-10 22:10:16 +0100

    Update download_datasets.sh
    Update dataset download

--- scripts/download_datasets.sh
+++ scripts/download_datasets.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Downlad the datasets from google drive if not already present
 if [ ! -d ./dataset ]; then
-    pip freeze | grep "^gdown" || pip install gdown
+    pip freeze | grep -q "^gdown" || pip install gdown
     gdown "1alE33S1GmP5wACMXaLu50rDIoVzBM4ik"
     unzip -n -j all_six_datasets.zip -d dataset
-    rm dataset/.*
+    rm dataset/.[!.]*
     rm all_six_datasets.zip
 fi
